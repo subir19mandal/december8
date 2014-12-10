19e387f36ca7be82af99b08690acccb84ed6bb6d
subir19mandal <subir.mandal19@yahoo.com>
2014-12-10 15:32:01 -0800

    new fill added

---
+++ Problem3LpfApp/Problem3LpfApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3LpfApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const long targetNumber = 600851475143;
+            var primeFactors = new List<long>();
+
+            for (long i = 1; i <= targetNumber; i++)
+            {
+                Console.WriteLine(i);
+                if (PrimeFactor(targetNumber, i))
+                {
+                    primeFactors.Add(i);
+                }
+            }
+
+            var biggestPrimeFactor = primeFactors.Max();
+
+            Console.WriteLine(biggestPrimeFactor);
+
+        }
+
+        public static bool PrimeFactor(long number, long i)
+        {
+            return number % i == 0;
+        }
+    }
+}
---
+++ ProblemFn2App/ProblemFn2App/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemFn2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double count, f1 = 0, f2 = 1, f3, sum = 0;
+            Console.Write("Enter the Limit : ");
+            count = double.Parse(Console.ReadLine());
+            for (; ; )
+            {
+                f3 = f1 + f2;
+                if (f3 >= count)
+                    break;
+                if ((f3 % 2) == 0)
+                {
+                    sum += f3;
+                }
+
+                f1 = f2;
+                f2 = f3;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
